1c4b8114786d3eea8e2e94edc9a39c0ec06ec672
= <ansariovesh@yahoo.in>
2024-11-28 07:52:59 +0530

    useDynamicFields hook added

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as useInfiniteScroll } from "./hooks/useInfiniteScroll";
 export { default as useRouteTitle } from "./hooks/useRouteTitle";
 export { default as useSmartOSearch } from "./hooks/useSmartOSearch";
+export { default as useDynamicFields } from "./hooks/useDynamicFields";
